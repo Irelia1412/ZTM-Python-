a058366e74b35bf9057eca800ca2eb90236fbae5
Mohammed <58148709+Irelia1412@users.noreply.github.com>
2019-12-11 13:05:45 +0300

    Update email_login_yes.py

--- Email/email_login_yes.py
+++ Email/email_login_yes.py
@@ -9,6 +9,6 @@ email.set_content("You are cute !" )
 with smtplib.SMTP(host='smtp.gmail.com', port = 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login("nphamood100@gmail.com" , "1246-hamood")
+    smtp.login("nphamood100@gmail.com" , "********")
     smtp.send_message(email)
     print("All good !")
